fdb43c095015642bb8f19a0e9297850eb5df2234
AngelDuocUC <107085182+AngelDuocUC@users.noreply.github.com>
2023-10-30 10:37:10 -0300

    Revert "v 4.0.1"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,5 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { register } from 'swiper/element/bundle';
-
-register();
 
 @Component({
   selector: 'app-root',
